c6976aef210f7b5490093dd0f3fc4e4c4672152e
Miloš Đurić <milossdjuric@yahoo.com>
2025-01-08 02:36:27 +0100

    update install.sh to forked repos

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ cd ..
 # todo: switch to c12s repo
 git clone https://github.com/c12s/kuiper
 git clone https://github.com/c12s/quasar
-git clone https://github.com/c12s/magnetar
-git clone https://github.com/c12s/star
+git clone https://github.com/milossdjuric/magnetar
+git clone https://github.com/milossdjuric/star
 git clone https://github.com/c12s/apollo
 git clone https://github.com/c12s/oort
 git clone https://github.com/c12s/gravity
